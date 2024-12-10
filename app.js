513d69cf2faef9882154bf780af65a6ef396869c
altairsf <altair.regina.26@gmail.com>
2024-12-10 00:35:23 -0300

    commit -m "React" -m "Updating"

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const cors = require('cors');
+const livroRouter = require('./routes/livros');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(cors());
+app.use('/livros', livroRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
